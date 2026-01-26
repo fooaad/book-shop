e9fae652cec811d64a87d49788d23155a0aef932
Fuad <fuad.mdfuadul@gmail.com>
2026-01-26 09:39:51 +0600

    Create router.go and wrap up with main.go
    Signed-off-by: Fuad <fuad.mdfuadul@gmail.com>

--- cmd/book-shop/main.go
+++ cmd/book-shop/main.go
@@ -1,13 +1,34 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"book-shop/internal/author"
+	"book-shop/internal/book"
+	"book-shop/internal/database"
+	"book-shop/internal/router"
+	"log"
+)
 
 func main() {
-  router := gin.Default()
-  router.GET("/ping", func(c *gin.Context) {
-    c.JSON(200, gin.H{
-      "message": "pong",
-    })
-  })
-  router.Run() // listens on 0.0.0.0:8080 by default
+	// Initialize database
+	db, err := database.NewDatabase()
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	// Initialize repositories
+	authorRepo := author.NewRepository(db)
+	bookRepo := book.NewRepository(db)
+
+	// Initialize services
+	authorService := author.NewService(authorRepo)
+	bookService := book.NewService(bookRepo)
+
+	// Initialize handlers
+	authorHandler := author.NewHandler(authorService)
+	bookHandler := book.NewHandler(bookService)
+
+	// Set up router
+	r := router.SetupRouter(authorHandler, bookHandler)
+
+	r.Run() // listens on 0.0.0.0:8080 by default
 }
